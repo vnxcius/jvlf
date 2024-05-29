f09e8b6ab57a8d1afc247c6dcbc90a03f70df48f
vnxcius <vsgh-gouveia@hotmail.com>
2024-05-29 09:39:33 -0300

    feat: create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+type Files = {
+  title: string,
+  slug: string,
+  url: string,
+  size: string,
+  type: string,
+}
+
+type People = {
+  img: string,
+  name: string,
+  abbreviation: string,
+}
